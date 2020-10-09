3d6ec45a79c865bf23af7cbd4818c42bab55fbf6
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-10-09 15:18:04 -0700

    added index new and show route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-
 const db = require('../models')
 
 
+
+//GET INDEX
 router.get('/', (req, res) => {
 
     db.User.find({}, (err, allUsers) => {
@@ -13,6 +14,28 @@ router.get('/', (req, res) => {
         res.render('users/index', context)
     })
     
+})
+
+//GET NEW
+
+router.get('/new', (req, res) => {
+    res.render('users/new')
+})
+
+//GET SHOW
+router.get('/:userId', (req, res) => {
+
+    db.User.findById(req.params.userId)
+    .populate('playlists')
+    .exec( (err, foundUser) => {
+        if(err) return console.log(err);
+        console.log('foundUser', foundUser)
+
+        const context = {
+            user : foundUser
+        }
+        res.render('users/show', context)
+    })
 })
 
 module.exports = router
