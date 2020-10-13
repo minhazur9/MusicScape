7441f084cd0cc83be6ed85fbf23bf9835a784677
min <minhazur9@gmail.com>
2020-10-13 02:03:12 -0400

    Made a confirmation prompt for deleting

---
+++ public/app.js
@@ -0,0 +1,19 @@
+
+// Event Listeners
+
+$('.delete-playlist').on('click', function() {
+    $('.index-front').css('filter', 'blur(8px)');
+    $('body').append(`<div class="warning"><h4>Are you sure? <br>It will be gone forever!</h4>
+    <input class="warning-button-no" type="submit" value="No..">
+    <input class="warning-button-yes" type="submit" value="Yes"></div>`);
+    $('#delete').appendTo('.warning')
+    $('.warning-button-yes').appendTo('#delete');
+    addCancel();
+})
+
+function addCancel() {
+    $('.warning-button-no').on('click', function() {
+        $('.warning').remove();
+        $('.index-front').css('filter', 'none');
+    })
+}
