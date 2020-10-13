14c2d7eaed3a7ca10d056f6a7ecc5d21a1677842
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-10-13 10:54:25 -0700

    added delete user popup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,10 @@
-
+$('#delete').hide()
 // Event Listeners
 
 $('.delete-playlist').on('click', function() {
+    $('#delete').show()
     $('.index-front').css('filter', 'blur(8px)');
+    $('#delete').show()
     $('body').append(`<div class="warning"><h4>Are you sure? <br>It will be gone forever!</h4>
     <input class="warning-button-no" type="submit" value="No..">
     <input class="warning-button-yes" type="submit" value="Yes"></div>`);
@@ -13,8 +15,9 @@ $('.delete-playlist').on('click', function() {
 
 function addCancel() {
     $('.warning-button-no').on('click', function() {
-        $('#delete').appendTo('body');
+        $('#delete').appendTo('body').hide();
         $('.warning').remove();
         $('.index-front').css('filter', 'none');
     })
 }
+
